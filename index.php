35cbc06b89cd3d16c6ed9d65500d5faffa277904
hamna4karimi <karimihamna6@gmail.com>
2022-05-11 15:47:27 +0500

    first folder

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+echo "Hello Students:"."<br>";
+$name="Ali";
+$lastname="Raza";
+
+echo $name."<br>";
+echo $lastname."<br>";
+
+//Arithmetic operators//
+$a=12;
+$b=4;
+$c=$a+$b;
+echo  "Add result is :".$c."<br>";
+
+$d=15;
+$e=5;
+$f=$d-$e;
+echo  "Sub res is :".$f."<br>";
+
+$g=7;
+$h=5;
+$i=$g=$h;
+echo "Multi res is :".$i."<br>";
+
+$j=5;
+$k=4;
+$l=$j/$k;
+echo "Divide answer is :".$l."<br>";
+
+$m=3;
+$n=9;
+$o=$m%$n;
+echo "Modulus res is :".$o."<br>";
+
+?>
+</body>
+</html>
